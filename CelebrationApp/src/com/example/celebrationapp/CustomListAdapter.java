698e75d363c79df199fbc0ccf8b6faba7d41f579
Derek Tyndall <dtyndal4@uncc.edu>
2013-11-29 14:40:51 -0500

    Solution worked. No longer using ID's from children count of view. Now using proper findViewById calls

--- CelebrationApp/src/com/example/celebrationapp/CustomListAdapter.java
+++ CelebrationApp/src/com/example/celebrationapp/CustomListAdapter.java
@@ -41,65 +41,44 @@ public class CustomListAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
     	ViewHolder holder;
     	
-    	TextView eventName = null; 
-    	TextView eventTime = null; 
-    	TextView authorName = null; 
-    	TextView eventLocation = null;
     	holder = new ViewHolder();
+    	
     	if (convertView == null) {
     		
     		  convertView = mInflater.inflate(R.layout.event_single, null);
-    		 
-    		  for(int i=0; i<((ViewGroup)convertView).getChildCount(); ++i) {
-    			  
-	    			  View nextChild =((ViewGroup)convertView).getChildAt(i);
-	    			  TextView next;
-	    			  next = (TextView) nextChild;
-	    			  //System.out.println("ID OF CHILD " + next.getId());
-	    			 if(next.getId() == 2131296276){
-	    				 eventName = next;
-	    			 }
-	    			 if(next.getId() ==  2131296277){
-	    				 eventTime = next;
-	    			 }
-	    			 if(next.getId() == 2131296278){
-	    				 authorName = next;
-	    			 }
-	    			 if(next.getId() == 2131296261){
-	    				 eventLocation = next;
-	    			 }
-    			
-    			}
-    		  
-    		  HashMap<String, String> currentData = new HashMap<String, String>();
-              currentData = list.get(position);
-              //System.out.println(currentData);
-             
-              eventName.setText(currentData.get("event_name"));
-              authorName.setText(currentData.get("author_name"));
-              eventTime.setText(currentData.get("event_time"));
-              
-              eventLocation.setText(currentData.get("event_location"));
-              if(currentData.get("track").equals("Leadership")){
-            	 eventName.setTextColor(0xffff0000);
-              }
-              if(currentData.get("track").equals("Civic Engagement")){
-            	  eventName.setTextColor(0xffff00ff);
-              }
-              if(currentData.get("track").equals("Corps Practices")){
-            	  eventName.setTextColor(0xff0000ff);
-              }
-              if(currentData.get("track").equals("Technical Excellence")){
-            	  eventName.setTextColor(0xffffc800);
-              }
               
     		  convertView.setTag(holder);
     		 } else {
     		  holder = (ViewHolder) convertView.getTag();
     		 }
-    	  
-		  holder.eventName = (TextView) convertView.findViewById(R.id.event_name);
-
+    	
+    	
+    	  TextView eventName = (TextView) convertView.findViewById(R.id.eventName);
+		  TextView authorName = (TextView) convertView.findViewById(R.id.authorName);
+		  TextView eventTime = (TextView) convertView.findViewById(R.id.eventTime);
+		  TextView eventLocation = (TextView) convertView.findViewById(R.id.eventLocation);
+		  
+		  HashMap<String, String> currentData = new HashMap<String, String>();
+          currentData = list.get(position);
+          //System.out.println(currentData);
+         
+          eventName.setText(currentData.get("event_name"));
+          authorName.setText(currentData.get("author_name"));
+          eventTime.setText(currentData.get("event_time"));
+          
+          eventLocation.setText(currentData.get("event_location"));
+          if(currentData.get("track").equals("Leadership")){
+        	 eventName.setTextColor(0xffff0000);
+          }
+          if(currentData.get("track").equals("Civic Engagement")){
+        	  eventName.setTextColor(0xffff00ff);
+          }
+          if(currentData.get("track").equals("Corps Practices")){
+        	  eventName.setTextColor(0xff0000ff);
+          }
+          if(currentData.get("track").equals("Technical Excellence")){
+        	  eventName.setTextColor(0xffffc800);
+          }
     	  
     	 return convertView;
     	}
